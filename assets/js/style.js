d3558cecb024e0a488dfe13c4b2d257b449e7ba1
NaifatSelina <naifatselina@icloud.com>
2022-06-02 23:48:10 +0000

    update class and id names in css

--- assets/js/style.js
+++ assets/js/style.js
@@ -227,12 +227,13 @@ const questions = [
 
 ]
 
-// empty array to hold shuffled questions
-let shuffledQuestions = []
 
-// function to ensure 10 shuffled questions are picked each time
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
 function handleQuestions() { 
-
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
     while (shuffledQuestions.length <= 9) {
         const random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)) {
@@ -240,22 +241,21 @@ function handleQuestions() {
         }
     }
 }
-// what question user is on
-let questionNumber = 1 
-// what the users score is
-let playerScore = 0  
-// how many questions have they gotten wrong
-let wrongAttempt = 0
-// display next question
-let indexNumber = 0
 
-// display the next question 
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
 function NextQuestion(index) {
     handleQuestions()
     const currentQuestion = shuffledQuestions[index]
     document.getElementById("question-number").innerHTML = questionNumber
     document.getElementById("player-score").innerHTML = playerScore
-    document.getElementById("displayed-question").innerHTML = currentQuestion.question;
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
     document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
     document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
     document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
@@ -263,11 +263,11 @@ function NextQuestion(index) {
 
 }
 
-// checks answer 
+
 function checkForAnswer() {
-    const currentQuestion = shuffledQuestions[indexNumber] 
-    const currentQuestionAnswer = currentQuestion.correctOption
-    const options = document.getElementsByName("option"); 
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
     let correctOption = null
 
     options.forEach((option) => {
@@ -277,38 +277,57 @@ function checkForAnswer() {
         }
     })
 
-        //checking user chose answer or show required pop up
-        if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
-            document.getElementById('option-required').style.display = "flex"
-        }
-
-        //checking if selected answer is same as correct answer
-        options.forEach((option) => {
-            if (option.checked === true && option.value === currentQuestionAnswer) {
-                document.getElementById(correctOption).style.backgroundColor = "green"
-                playerScore++ 
-                indexNumber++
-                //set to delay question number till when next question loads
-                setTimeout(() => {
-                    questionNumber++
-                }, 1000)
-            }
-    
-            else if (option.checked && option.value !== currentQuestionAnswer) {
-                const wrongLabelId = option.labels[0].id
-                document.getElementById(wrongLabelId).style.backgroundColor = "red"
-                document.getElementById(correctOption).style.backgroundColor = "green"
-                wrongAttempt++  
-                indexNumber++
-                //set to delay question number till when next question loads
-                setTimeout(() => {
-                    questionNumber++
-                }, 1000)
-            }
-        })
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
     }
 
-    //sets answers background back to default after showing the right/wrong colors
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
 function resetOptionBackground() {
     const options = document.getElementsByName("option");
     options.forEach((option) => {
@@ -316,7 +335,7 @@ function resetOptionBackground() {
     })
 }
 
-// resets radio buttons for next question
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
 function unCheckRadioButtons() {
     const options = document.getElementsByName("option");
     for (let i = 0; i < options.length; i++) {
@@ -324,50 +343,48 @@ function unCheckRadioButtons() {
     }
 }
 
-// end of game
+// function for when all questions being answered
 function handleEndGame() {
     let remark = null
     let remarkColor = null
 
     // condition check for player remark and remark color
     if (playerScore <= 3) {
-        remark = "Oh No! Are you a newbie? well keep practicing and I'm sure you'll do better next time."
+        remark = "Bad Grades, Keep Practicing."
         remarkColor = "red"
     }
     else if (playerScore >= 4 && playerScore < 7) {
-        remark = "Good score but we know you know Peter better than that!, Better luck next time."
+        remark = "Average Grades, You can do better."
         remarkColor = "orange"
     }
     else if (playerScore >= 7) {
-        remark = "Excellent! Marvel need to recruite you as a Spider-Man variant in the Multiverse at this rate!"
+        remark = "Excellent, Keep the good work going."
         remarkColor = "green"
     }
     const playerGrade = (playerScore / 10) * 100
 
     //data to display to score board
-    document.getElementById('feedback').innerHTML = remark
-    document.getElementById('feedback').style.color = remarkColor
-    document.getElementById('score-percentage').innerHTML = playerGrade
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
     document.getElementById('wrong-answers').innerHTML = wrongAttempt
     document.getElementById('right-answers').innerHTML = playerScore
-    document.getElementById('score-board').style.display = "flex"
+    document.getElementById('score-modal').style.display = "flex"
 
 }
 
-//close score board, resets game and reshuffles questions
-function closeScoreBoard() {
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
     questionNumber = 1
     playerScore = 0
     wrongAttempt = 0
     indexNumber = 0
     shuffledQuestions = []
     NextQuestion(indexNumber)
-    document.getElementById('score-board').style.display = "none"
+    document.getElementById('score-modal').style.display = "none"
 }
 
 //function to close warning modal
-function closeRequiredPage() {
-    document.getElementById('option-required').style.display = "none"
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
 }
-
-
