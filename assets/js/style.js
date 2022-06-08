495d093b2caa7140ed4c1d9fa5f27fba1ef878a6
NaifatSelina <naifatselina@icloud.com>
2022-06-08 22:59:24 +0000

    Add javascript to loop audio

--- assets/js/style.js
+++ assets/js/style.js
@@ -396,3 +396,15 @@ document.getElementById('button-audio').onclick = function() {
     audio.play();
 }
 
+if (typeof audio.loop == 'boolean')
+{
+    audio.loop = true;
+}
+else
+{
+    audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+
